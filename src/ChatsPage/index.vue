3e78c308a6269eef1a7438f6d870e76176d5107e
GABRIEL DOMINGUES RIBEIRO <22283347@dsn127m1241959.cfp127aluno.local>
2023-11-08 09:03:18 -0300

    botao voltar e botao abrir batepapo

--- src/ChatsPage/index.vue
+++ src/ChatsPage/index.vue
@@ -1,43 +1,68 @@
 <template>
-    <div style="height: 100vh">
-      <PrettyChatWindow
-        :projectId="projectId"
-        :username="username"
-        :secret="secret"
-      />
-    </div>
-  </template>
-  
-  <style>
-  .ce-new-chat-button {
-    width: 32px;
-    position: relative;
-    bottom: 22px;
-  }
-  </style>
-  
-  <script>
-  import { PrettyChatWindow } from "react-chat-engine-pretty";
-  import { applyReactInVue } from "veaury";
-  
-  export default {
-    data() {
-      return {
-        projectId: import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID,
-      };
+  <div style="height: 100vh">
+    <!-- Botão flutuante para abrir a sala de bate-papo -->
+    <button @click="toggleChat" class="ce-new-chat-button" v-if="!chatVisible">Abrir Sala de Bate-Papo</button>
+    
+    <!-- Sala de bate-papo visível somente se chatVisible for verdadeiro -->
+    <PrettyChatWindow
+      v-if="chatVisible"
+      :projectId="projectId"
+      :username="username"
+      :secret="secret"
+    />
+
+    <!-- Botão "Voltar" no canto inferior esquerdo -->
+    <button @click="goBack" class="ce-back-button" v-if="chatVisible">Voltar</button>
+  </div>
+</template>
+
+<style>
+.ce-new-chat-button {
+  width: 32px;
+  position: fixed;
+  bottom: 22px;
+  right: 22px; /* Posição flutuante no canto inferior direito */
+}
+
+.ce-back-button {
+  width: 32px;
+  position: fixed;
+  bottom: 22px;
+  left: 22px; /* Posição flutuante no canto inferior esquerdo */
+}
+</style>
+
+<script>
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+import { applyReactInVue } from "veaury";
+
+export default {
+  data() {
+    return {
+      projectId: import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID,
+      chatVisible: false, // Inicialmente oculto
+    };
+  },
+  components: {
+    PrettyChatWindow: applyReactInVue(PrettyChatWindow),
+  },
+  props: {
+    username: {
+      type: String,
+      required: true,
     },
-    components: {
-      PrettyChatWindow: applyReactInVue(PrettyChatWindow),
+    secret: {
+      type: String,
+      required: true,
     },
-    props: {
-      username: {
-        type: String,
-        required: true,
-      },
-      secret: {
-        type: String,
-        required: true,
-      },
+  },
+  methods: {
+    toggleChat() {
+      this.chatVisible = true;
     },
-  };
-  </script>
+    goBack() {
+      this.chatVisible = false; // Oculta a sala de bate-papo ao voltar
+    },
+  },
+};
+</script>
